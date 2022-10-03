11312b245c8916ca0c25c9d7d1ed8ee91400b75a
Santhosh Kurnapally <santhoshkurnapally@Santhoshs-MacBook-Air.local>
2022-10-03 13:22:38 +0530

    updated background

--- final.py
+++ final.py
@@ -4,6 +4,23 @@ import numpy as np
 import time
 from PIL import Image
 import create_model as cm
+import base64
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('./Background/1.jpg')    
 
 
 st.title("Chest X-ray Captioning")
