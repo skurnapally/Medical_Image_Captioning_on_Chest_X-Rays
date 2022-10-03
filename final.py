52b6ab6d997a9f9d14f0c7aefe07d639c00f28f4
Santhosh Kurnapally <santhoshkurnapally@Santhoshs-MacBook-Air.local>
2022-10-03 16:45:30 +0530

    updated title color

--- final.py
+++ final.py
@@ -35,7 +35,6 @@ st.markdown(note,unsafe_allow_html=True)
 col1,col2 = st.beta_columns(2)
 predict_button = col1.button('Predict on uploaded files')
 test_data = col2.button('Predict on sample data')
-predict_uploaded = False
 image_1 = None
 image_2 = None
 if predict_button:
@@ -48,58 +47,3 @@ if predict_button:
 		st.markdown(secondxray,unsafe_allow_html=True)
 		image_2 = st.file_uploader(label = " ",type=['png','jpg','jpeg'])
 	predict_uploaded = st.button('Predict')
-
-	@st.cache
-	def create_model():
-		model_tokenizer = cm.create_model()
-		return model_tokenizer
-
-
-	def predict(image_1,image_2,model_tokenizer,predict_uploaded = predict_uploaded):
-    	start = time.process_time()
-    	if predict_uploaded:
-        	if (image_1 is not None):
-        		start = time.process_time()  
-            	image_1 = Image.open(image_1).convert("RGB") #converting to 3 channels
-            	image_1 = np.array(image_1)/255
-            	if image_2 is None:
-                	image_2 = image_1
-            	else:
-                	image_2 = Image.open(image_2).convert("RGB") #converting to 3 channels
-                	image_2 = np.array(image_2)/255
-            	st.image([image_1,image_2],width=300)
-            	caption = cm.function1([image_1],[image_2],model_tokenizer)
-            	st.markdown(" ### **Findings:**")
-            	findings = st.empty()
-            	findings.write(caption[0])
-            	time_taken = "Time Taken for prediction: %i seconds"%(time.process_time()-start)
-            	st.write(time_taken)
-            	del image_1,image_2
-        	else:
-            	st.markdown("## Upload an Image")
-    model_tokenizer = create_model()
-    predict(image_1,image_2,model_tokenizer)
-    
-    
-if testdata:
-	def predict_sample(model_tokenizer,folder = './test_images'):
-    	no_files = len(os.listdir(folder))
-    	file = np.random.randint(1,no_files)
-    	file_path = os.path.join(folder,str(file))
-    	if len(os.listdir(file_path))==2:
-        	image_1 = os.path.join(file_path,os.listdir(file_path)[0])
-        	image_2 = os.path.join(file_path,os.listdir(file_path)[1])
-        	print(file_path)
-    	else:
-        	image_1 = os.path.join(file_path,os.listdir(file_path)[0])
-        	image_2 = image_1
-    	predict(image_1,image_2,model_tokenizer,True)
-    model_tokenizer = create_model()
-    predict_sample(model_tokenizer)
-
-
-
-
-
-
-
